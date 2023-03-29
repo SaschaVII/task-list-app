e5a21d1663cc90a0180fe563ca4fcebb62c110e0
Sascha Kuhness <skuhness@calendarclub.co.uk>
2023-03-29 09:51:13 +0100

    Add onClick prop to button component

--- components/button.js
+++ components/button.js
@@ -1,8 +1,8 @@
-const Button = ({ children, className }) => {
+const Button = ({ children, className, onClick }) => {
     const styles = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 " + className;
 
     return (
-        <button className={styles}>
+        <button className={styles} onClick={onClick}>
             {children}
         </button>
     );
