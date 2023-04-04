cc049aca1d3c27c39c79e7fc67a3a13eff3b78bc
Sascha Kuhness <skuhness@calendarclub.co.uk>
2023-04-04 14:03:07 +0100

    Add type prop to button component
    Additional Changes:
    - Add "danger" type to button component

--- components/button.js
+++ components/button.js
@@ -1,5 +1,10 @@
-const Button = ({ children, className, onClick }) => {
-    const styles = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 " + className;
+const Button = ({ children, className, onClick, type }) => {
+    const stylesPrimary = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2";
+    const stylesDanger = "text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2";
+    let styles = stylesPrimary;
+    if (type === "danger") { styles = stylesDanger; }
+
+    styles += " " + className;
 
     return (
         <button className={styles} onClick={onClick}>
