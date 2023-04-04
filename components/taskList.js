a24d73f391d489e9a21cf131f765db687d2e0edd
Sascha Kuhness <skuhness@calendarclub.co.uk>
2023-04-04 09:09:05 +0100

    Remove dummy tasks and show add button when list empty

--- components/taskList.js
+++ components/taskList.js
@@ -63,7 +63,6 @@ const TaskList = () => {
         router.push(`/taskDetails?id=${taskId}`);
     };
 
-    const handleFillDummyDataClick = () => fillDummyList();
     const handleAddTaskClick = () => addTask();
 
     // * Helper Functions
@@ -71,65 +70,6 @@ const TaskList = () => {
         const dateA = new Date(a.dueDate);
         const dateB = new Date(b.dueDate);
         return dateA - dateB;
-    };
-
-    const fillDummyList = () => {
-        setTaskList(
-            [{
-                "id": 0,
-                "title": "Complete Task List App",
-                "description": "Finish building and testing the task list app by end of week. Tempor nulla ullamco proident officia dolore veniam nisi Lorem aute in et ipsum adipisicing cillum. Eiusmod esse et cupidatat nostrud quis in. Ea aliqua ex eu laboris nostrud duis pariatur aute excepteur pariatur qui adipisicing occaecat. Fugiat occaecat ea elit nostrud voluptate. Pariatur adipisicing minim culpa velit. Laboris ut proident ex et aliquip veniam est laborum labore aute reprehenderit ex tempor.",
-                "completed": false,
-                "dueDate": "2023-03-19",
-                "priority": 1,
-                "focused": false
-            },
-            {
-                "id": 1,
-                "title": "Prepare for Interview",
-                "description": "Research the company, practice answering interview questions, and dress professionally",
-                "completed": true,
-                "dueDate": "2023-03-22",
-                "priority": 2,
-                "focused": false
-            },
-            {
-                "id": 2,
-                "title": "Go to the Gym",
-                "description": "Do 30 minutes of cardio and 20 minutes of weight lifting",
-                "completed": false,
-                "dueDate": "2023-03-17",
-                "priority": 2,
-                "focused": false
-            },
-            {
-                "id": 3,
-                "title": "Buy Groceries",
-                "description": "Get bread, milk, eggs, and fruit",
-                "completed": false,
-                "dueDate": "2023-03-16",
-                "priority": 3,
-                "focused": false
-            },
-            {
-                "id": 4,
-                "title": "Fix S1 Bugs",
-                "description": "Fix all bugs listed on ClickUp for the S1 Website",
-                "completed": false,
-                "dueDate": "2023-04-10",
-                "priority": 1,
-                "focused": false
-            },
-            {
-                "id": 5,
-                "title": "Read a Book",
-                "description": "Read at least one chapter of 'The Catcher in the Rye'",
-                "completed": true,
-                "dueDate": "2023-03-18",
-                "priority": 2,
-                "focused": false
-            }]
-        );
     };
 
     const addTask = () => {
@@ -159,10 +99,9 @@ const TaskList = () => {
 
     if (!taskList || taskList.length === 0) return (
         <>
-            <Button onClick={handleFillDummyDataClick}>
-                <FontAwesomeIcon className="mr-2 -ml-1" icon={faWandMagicSparkles} />
-                Fill with Dummy Tasks
-            </Button>
+            <div className="px-5 py-4 flex items-center space-x-4 cursor-pointer hover:bg-gray-200" onClick={handleAddTaskClick}>
+                <FontAwesomeIcon className="mr-2 -ml-1 grow text-2xl" icon={faPlus} />
+            </div>
         </>
     );
     return (
